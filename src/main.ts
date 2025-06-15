46e9763344b7c1c06b255dfe0812ab434d8b4ae6
javierm-0 <javier.moraga@alumnos.ucn.cl>
2025-06-15 18:27:53 -0400

    incluye CORS

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,12 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: process.env.FRONTEND_IP,
+    methods: ['GET', 'POST','PATCH'],
+    allowedHeaders: ['Content-Type'],
+  })
+
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true,
     forbidNonWhitelisted: true,
