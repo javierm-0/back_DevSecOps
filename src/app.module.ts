d67385827b8f6ea36e7403c108d653ec6a87812c
javierm-0 <javier.moraga@alumnos.ucn.cl>
2025-06-20 14:30:25 -0400

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,8 +4,6 @@ import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
 
-
-import { UserService } from './users/user.service';
 import { UserController } from './users/user.controller';
 import { UserModule } from './users/user.module';
 import { AuthModule } from './auth/auth.module';
